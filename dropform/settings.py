9cd11c43e7879fdbbd440f7d53722fea6bf55ebb
loharvikas <loharvikas13@gmail.com>
2022-01-09 18:32:41 +0530

    Minor changes

--- dropform/settings.py
+++ dropform/settings.py
@@ -217,7 +217,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "http://dropform.co",
+    "http://www.dropform.co",
 ]
 
 # CELERY CONFIGURATIONS
